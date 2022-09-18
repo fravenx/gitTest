0610bfa1869609a1fafbf846bcb0f95e2cc342a7
fraven <fraven@qq.com>
2022-09-18 20:49:08 +0800

    push test

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git master");
         System.out.println("hello git hot fix");
+        System.out.println("hello git push");
     }
 }
